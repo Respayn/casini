987d4ce171525b8617b43a47db3a303a52563292
Krasyuk Maksim <krasyuk@softorium.pro>
2025-03-06 01:35:00 +0300

    Определение формы без соответствия типов полей

---
+++ resources/views/components/menu/back-button.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <a href="javascript:void(0);" onclick="window.history.back()" class="text-secondary-text inline-flex gap-3 items-center max-h-[26px] text-[18px]">
+        <x-icons.arrow-left/>
+        <span>
+            Назад
+        </span>
+    </a>
+</div>
--- resources/views/livewire/system-settings/client-and-porjects/create-client-project.blade.php
+++ resources/views/livewire/system-settings/client-and-porjects/create-client-project.blade.php
@@ -1,4 +1,138 @@
 <div>
-    <h1>Создание клиенто-проекта</h1>
+    <x-menu.back-button/>
+    <div class="flex flex-col gap-4 mt-4">
+        <h1>Добавить клиенто-проект</h1>
+        <x-form.form>
+            <div class="flex flex-col gap-4">
+                <h2>Основная информация</h2>
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Статус клиенто-проекта</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
 
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Название клиенто-проекта</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Выберите клиента</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >URL-адрес сайта</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                    >Менеджер</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Специалист</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                    >Помощники</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >KPI</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Отдел</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Тип клиенто-проекта</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                    >Свой проект</x-form.form-label>
+                    <div>
+                        <x-form.input-text wire:model=""></x-form.input-text>
+                    </div>
+                </x-form.form-field>
+            </div>
+            <div class="flex flex-col gap-4">
+                <h2>Показатели по рынку</h2>
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Регион продвижения</x-form.form-label>
+                    <div>
+                        <x-form.select wire:model=""></x-form.select>
+                    </div>
+                </x-form.form-field>
+                <x-form.form-field>
+                    <x-form.form-label
+                        class="self-baseline"
+                        required
+                    >Тематика продвижения</x-form.form-label>
+                    <div>
+                        <x-form.select wire:model=""></x-form.select>
+                    </div>
+                </x-form.form-field>
+            </div>
+        </x-form.form>
+    </div>
 </div>
