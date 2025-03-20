cf50c94ae4686fc5e72233dca6f84f91ffff99fb
Krasyuk Maksim <krasyuk@softorium.pro>
2025-03-20 10:55:51 +0300

    feat(ui): Надпись "Данные не определены" (таблица)

--- resources/views/components/data/table-rows.blade.php
+++ resources/views/components/data/table-rows.blade.php
@@ -1,3 +1,25 @@
+@props([
+    'emptyMessage' => null
+])
+
 <tbody {{ $attributes }}>
-    {{ $slot }}
+    @php
+        $renderedSlot = $slot->toHtml();
+
+        $renderedSlotWithoutComments = preg_replace('/<!--(.|\s)*?-->/', '', $renderedSlot);
+
+        $renderedSlotTrimmed = trim($renderedSlotWithoutComments);
+
+        $isSlotEmpty = ($renderedSlotTrimmed === '');
+    @endphp
+
+    @if ($isSlotEmpty)
+        <x-data.table-row>
+            <x-data.table-cell colspan="1000" class="italic">
+                {{ $emptyMessage ?? 'Данные отсутствуют' }}
+            </x-data.table-cell>
+        </x-data.table-row>
+    @else
+        {!! $renderedSlot !!}
+    @endif
 </tbody>
