56685703984112d84b773fe1eafe86a5b7efd0b0
Krasyuk Maksim <krasyuk@softorium.pro>
2025-03-20 11:13:48 +0300

    feat(ui): ИНН, уникальность и определение формата

--- app/Livewire/Forms/SystemSettings/ClientAndProjects/ClientForm.php
+++ app/Livewire/Forms/SystemSettings/ClientAndProjects/ClientForm.php
@@ -12,7 +12,7 @@ class ClientForm extends Form
     #[Rule('required|string|max:255', message: 'Название клиента обязательно')]
     public string $name = '';
 
-    #[Rule('required|string|max:12', message: 'ИНН обязателен и должен содержать до 12 символов')]
+    #[Rule('required|regex:/^\d{10,12}$/|unique:clients,inn,id', message: 'Некорректный формат ИНН')]
     public string $inn = '';
 
     #[Rule('required|exists:users,id', message: 'Выберите менеджера')]
