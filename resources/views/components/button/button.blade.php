7e5881172d8d7bfdbb22843621405d4666989b59
Aleksandr Syrtsev <syrtsev@softorium.pro>
2025-09-29 12:10:18 +0700

    fix: code style of button component

--- resources/views/components/button/button.blade.php
+++ resources/views/components/button/button.blade.php
@@ -8,7 +8,7 @@
     'size' => null,
     'iconClasses' => '',
     'square' => null,
-    'type' => 'button'
+    'type' => 'button',
 ])
 
 @php
@@ -18,19 +18,24 @@
 
     // Базовые классы, применяемые ко всем кнопкам
     $buttonClasses = [
-        'inline-flex','gap-2','items-center','justify-center',
-        'cursor-pointer','disabled:cursor-not-allowed','transition'
+        'inline-flex',
+        'gap-2',
+        'items-center',
+        'justify-center',
+        'cursor-pointer',
+        'disabled:cursor-not-allowed',
+        'transition',
     ];
 
     // Классы, зависящие от варианта кнопки
     $buttonClasses[] = match ($variant) {
         'primary' => 'hover:not-disabled:bg-transparent bg-primary border-primary disabled:text-default-button-disabled hover:not-disabled:text-primary disabled:bg-secondary border text-white disabled:border-0',
-        'ghost'   => 'text-default-button hover:not-disabled:bg-default-button disabled:text-default-button-disabled hover:not-disabled:text-white disabled:bg-secondary',
-        'link'    => 'text-primary-text hover:text-primary items-center group hover:underline',
-        'action'  => 'text-primary items-center group relative font-semibold relative',
-        'implicit-action'  => 'text-secondary-text items-center group relative font-semibold relative',
+        'ghost' => 'text-default-button hover:not-disabled:bg-default-button disabled:text-default-button-disabled hover:not-disabled:text-white disabled:bg-secondary',
+        'link' => 'text-primary-text hover:text-primary items-center group hover:underline',
+        'action' => 'text-primary items-center group relative font-semibold relative',
+        'implicit-action' => 'text-secondary-text items-center group relative font-semibold relative',
         'outlined' => 'border border-input-border hover:border-primary active:bg-primary active:border-primary group text-secondary-text hover:text-primary active:text-white',
-        default   => 'border-default-button text-default-button hover:not-disabled:bg-default-button disabled:text-default-button-disabled hover:not-disabled:text-white disabled:bg-secondary border disabled:border-0',
+        default => 'border-default-button text-default-button hover:not-disabled:bg-default-button disabled:text-default-button-disabled hover:not-disabled:text-white disabled:bg-secondary border disabled:border-0',
     };
 
     // TODO: добавить severity для изменения цветовой темы. Пример - https://primevue.org/button/#severity
@@ -48,27 +53,48 @@
 @endphp
 
 @if ($href)
-    <a href="{{ $href }}"
-       {{ $attributes->merge(['class' => implode(' ', $buttonClasses)]) }}
-       wire:navigate
+    <a
+        href="{{ $href }}"
+        {{ $attributes->merge(['class' => implode(' ', $buttonClasses)]) }}
+        wire:navigate
     >
-        @if ($icon) <x-dynamic-component class="{{ $iconClasses }}" :component="$icon" /> @endif
-        @if ($label) <span>{{ $label }}</span> @endif
+        @if ($icon)
+            <x-dynamic-component
+                class="{{ $iconClasses }}"
+                :component="$icon"
+            />
+        @endif
+        @if ($label)
+            <span>{{ $label }}</span>
+        @endif
     </a>
 @else
-    <button type="{{ $type }}"
-            {{ $attributes->merge(['class' => implode(' ', $buttonClasses)]) }}
-            @if ($disabled) disabled @endif>
-        @if ($icon) <x-dynamic-component class="{{ $iconClasses }}" :component="$icon" /> @endif
-        @if($label)
+    <button
+        type="{{ $type }}"
+        {{ $attributes->merge(['class' => implode(' ', $buttonClasses)]) }}
+        @if ($disabled) disabled @endif
+    >
+        @if ($icon)
+            <x-dynamic-component
+                class="{{ $iconClasses }}"
+                :component="$icon"
+            />
+        @endif
+        @if ($label)
             <span class="relative">
-              <span class="{{ $variant === 'action' ? 'whitespace-nowrap' : '' }}">{{ $label }}</span>
-              @if ($variant === 'action')
-                <span class="absolute left-0 right-0 bottom-[2px] rounded-xl border-b border-primary" style="border-width: .5px;"></span>
-              @endif
-              @if ($variant === 'implicit-action')
-                <span class="absolute left-0 right-0 bottom-[2px] rounded-xl border-b border-secondary-text" style="border-width: .5px;"></span>
-              @endif
+                <span class="{{ $variant === 'action' ? 'whitespace-nowrap' : '' }}">{{ $label }}</span>
+                @if ($variant === 'action')
+                    <span
+                        class="border-primary absolute bottom-[2px] left-0 right-0 rounded-xl border-b"
+                        style="border-width: .5px;"
+                    ></span>
+                @endif
+                @if ($variant === 'implicit-action')
+                    <span
+                        class="border-secondary-text absolute bottom-[2px] left-0 right-0 rounded-xl border-b"
+                        style="border-width: .5px;"
+                    ></span>
+                @endif
             </span>
         @endif
     </button>
