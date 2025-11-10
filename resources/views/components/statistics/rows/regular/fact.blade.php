b318c930a7fa6adbb67ad3dc0456969b9e052f37
Aleksandr Syrtsev <syrtsev@softorium.pro>
2025-11-10 09:35:17 +0700

    fix: layout bug in fact cell
    Refs: https://yt.softorium.pro/issue/SEO-202/Obnovlenie-Statistika

--- resources/views/components/statistics/rows/regular/fact.blade.php
+++ resources/views/components/statistics/rows/regular/fact.blade.php
@@ -12,7 +12,7 @@
 {{-- Установка высоты ячейки в маленькое значение - хак
 https://stackoverflow.com/questions/3542090/how-to-make-div-fill-td-height--}}
 <x-data.table-cell {{ $attributes }} class="!p-0 h-1">
-    <div class="grid grid-flow-col auto-rows-auto h-full divide-y divide-table-cell">
+    <div class="grid auto-rows-auto h-full divide-y divide-table-cell">
         @foreach ($parameters as $parameter)
             <div class="grid grid-cols-2 divide-x divide-table-cell">
                 <div class="flex items-center ps-2.5">{{ $parameter['plan'] }}</div>
